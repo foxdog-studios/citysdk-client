8b8dee05e0288d6cfb1973218988f3ca97f5a614
Peter Sutton <foxxy@foxdogstudios.com>
2014-02-08 20:35:02 +0000

    Moved source to client directory

--- lib/citysdk/client.rb
+++ lib/citysdk/client.rb
@@ -4,7 +4,7 @@ require 'addressable/uri'
 require 'faraday'
 
 require_relative 'client/api.rb'
-require_relative 'citysdk/importer'
+require_relative 'client/importer'
 require_relative 'client/node_builder.rb'
-require_relative 'citysdk/node_iterator'
+require_relative 'client/node_iterator'
 
